00cca94a4e5ff2f25a05d72b75b11b7e2065f757
KumareshRamachandran <kumareshram123@gmail.com>
2025-01-15 15:11:04 +0530

    Changes

--- Template.cpp
+++ Template.cpp
@@ -1,89 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64    RNG(chrono::steady_clock::now().time_since_epoch().count());
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define Yes cout << "YES" << nline
-#define No cout << "NO" << nline
-#define fall(x, n) for(int x=0; x<n; x++)
-#define rall(x, n) for(int x=n-1; x>=0; x--)
-#define frange(x, start, end) for(int x=start; x<=end; x++)
-#define rrange(x, start, end) for(int x=start; x>=end; x--)
-#define set_bits __builtin_popcountll
-#define vi vector<int>
-#define vii vector<vector<int>>
-#define ii pair<int, int>
-#define initValues(i, v) for(auto &i: v) cin >> i;
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
+#define       fastio()                  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define       nline                     "\n"
+#define       pb                        push_back
+#define       ppb                       pop_back
+#define       mp                        make_pair
+#define       ff                        first
+#define       ss                        second
+#define       fall(x, n)                for(int x=0; x<n; x++)
+#define       rall(x, n)                for(int x=n-1; x>=0; x--)
+#define       frange(x, start, end)     for(int x=start; x<=end; x++)
+#define       rrange(x, start, end)     for(int x=start; x>=end; x--)
+#define       vi                        vector<int>
+#define       ii                        pair<int, int>
+#define       initValues(i, v)          for(auto &i: v) cin >> i;
+#define       sz(x)                     ((int)(x).size())
+#define       all(x)                    (x).begin(), (x).end()
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+typedef       long long                 ll;
+typedef       unsigned long long        ull;
+typedef       long double               lld;
 
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#ifndef       ONLINE_JUDGE
+#include      "debug.h"
 #else
-#define debug(x)
+#define       debug(x)
 #endif
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V, class Comp = less<T>> void _print(const map<T, V, Comp> v) {cerr << "[ "; for (auto i : v) {cerr << "{" << i.ff << ", " << i.ss << "} ";} cerr << "]";}
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-
+#define       int                       long long
 
 void solve()
 {
-
+    int n = rand() % 5 + 1, m = rand() % 5 + 1;
+    cout << n << " " << m << "new\n";
+    fall(i, n) {
+        fall(j, m)
+        cout << (rand() % 10) << " \n"[j == m - 1];
+    }
 }
-
 
 signed main()
 {
 #ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	freopen("error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout); freopen("error.txt", "w", stderr);
 #endif
-	auto begin = std::chrono::high_resolution_clock::now();
-	fastio();
-	int t = 1;
-	cin >> t;
-	cin.ignore();
-	while (t--)
-	{
-		solve();
-	}
-	auto end = std::chrono::high_resolution_clock::now();
-	auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-	cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
-	return 0;
+    auto begin = chrono::high_resolution_clock::now(); fastio();
+    int t = 1;
+    cin >> t; cin.ignore();
+    while (t--) solve();
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
 }
-
---
+++ debug.h
@@ -0,0 +1,28 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V, class Comp = less<T>> void _print(const map<T, V, Comp> v) {cerr << "[ "; for (auto i : v) {cerr << "{" << i.ff << ", " << i.ss << "} ";} cerr << "]";}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+
+#endif
